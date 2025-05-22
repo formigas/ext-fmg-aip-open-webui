7663889389d9ab420fdcc681ccfe5bbd55e2e81c
Florian Halter <florianhalter@web.de>
2025-05-22 13:59:17 +0200

    fix: pipeline

--- src/routes/+layout.server.ts
+++ src/routes/+layout.server.ts
@@ -2,9 +2,9 @@ import type { LayoutServerLoad } from './$types';
 import { loadCustomThemesFromEnv } from '$lib/server/custom-theme-loader';
 
 export const load: LayoutServerLoad = async () => {
-  const themes = loadCustomThemesFromEnv();
+	const themes = loadCustomThemesFromEnv();
 
-  return {
-    customThemes: themes ?? {},
-  };
+	return {
+		customThemes: themes ?? {}
+	};
 };
